3eeea13b289cd87bd72a222dce1b65988bebf319
Jonathon Byrdziak <jonathonbyrdziak@Jonathons-MacBook-Air.local>
2021-11-17 11:25:43 -0600

    Fixes jonathonbyrdziak/laravel-code-challenge#3 Add Owner Info To Car Table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,5 +19,6 @@ class DatabaseSeeder extends Seeder
             
         $this->call(OwnerTotalSeeder::class);
         $this->call(AddressesNamesSeeder::class);
+        $this->call(CarsNameSeeder::class);
     }
 }
